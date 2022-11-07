e10044ce6c01331e32470f770d103992ed38639c
Alfred Garraffa <agarraffa@gmail.com>
2022-11-07 10:50:12 -0700

    initial FE work

--- client/src/App.js
+++ client/src/App.js
@@ -2,43 +2,44 @@ import logo from './logo.svg';
 import React  from "react";
 import './App.css';
 // bring in the DB stuff from SQL
-import {
-  BrowserRouter as Router, 
-  Routes,
-  Route,
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router, 
+//   Routes,
+//   Route,
+// } from "react-router-dom";
 
 //import the pages (ie home, well search, etc);
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+        <h1>Hello World!</h1>
+      </header>
+    </div>
+  );
+}
 
 //consruct the SQL API endpoing using httpLink
 
-function App() {
+// function App() {
 
-  return (
-    <div> App goes here </div>
-  )
-}
+//   return (
+//     <div> App goes here </div>
+//   )
+// }
 
 
 
