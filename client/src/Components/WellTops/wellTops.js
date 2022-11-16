c33ad5f20636cd394f8af44db4d118407d3ba6b6
Alfred Garraffa <agarraffa@gmail.com>
2022-11-16 13:05:08 -0700

    fixed issue with updating tops state

--- client/src/Components/WellTops/wellTops.js
+++ client/src/Components/WellTops/wellTops.js
@@ -4,20 +4,22 @@ import React, { useState } from 'react';
 export default function WellTops() {
 
     // These are just for building out the page. In the final version the tops will populate based on the project tops.
-    let testTops = [
-        {name: 'Pierre'}, 
-        {name: 'Madison'},
-        {name: 'Kibby'}, 
-        {name: 'Lodgepole'}, 
-        {name: 'False Bakken'},
-        {name: 'UBS'}, 
-        {name: 'MBS'}, 
-        {name: 'LBS'}, 
-        {name: 'TF1'}, 
-        {name: 'TF2'}, 
-        {name: 'TF3'}, 
-        {name: 'Nisku'}
-        ];
+    // let testTops = [
+    //     {name: 'Pierre'}, 
+    //     {name: 'Madison'},
+    //     {name: 'Kibby'}, 
+    //     {name: 'Lodgepole'}, 
+    //     {name: 'False Bakken'},
+    //     {name: 'UBS'}, 
+    //     {name: 'MBS'}, 
+    //     {name: 'LBS'}, 
+    //     {name: 'TF1'}, 
+    //     {name: 'TF2'}, 
+    //     {name: 'TF3'}, 
+    //     {name: 'Nisku'}
+    //     ];
+
+    let testTops = ['Pierre', 'Madison', 'Kibby', 'Lodgepole', 'False Bakken', 'UBS', 'MBS', 'LBS', 'TF1', 'TF2', 'TF2', 'Nisku']
 
     const [formData, setFormData] = useState("");
 
@@ -26,6 +28,7 @@ export default function WellTops() {
         console.log(name);
         console.log(value);
         setFormData({ ...formData, [name]: value });
+        console.log(formData)
 
 
     };
@@ -47,13 +50,13 @@ export default function WellTops() {
                 {testTops.map((element, index) => {
                     return (
                         <div key={index}>
-                            <h3>{element.name}</h3> 
+                            <h3>{element}</h3> 
                             <input
                                 className="wellTops-input-form"
                                 type="text"
                                 placeholder="SSD"
                                 value={formData.element ?? ""}
-                                name={element.name}
+                                name={element}
                                 onChange={handleInputChange}
                             ></input>
                         </div>
